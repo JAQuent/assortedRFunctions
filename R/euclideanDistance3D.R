ccb129d5083f622c823ec0a9fa670456122aad4a
JAQuent <alexander.quent@rub.de>
2019-06-11 12:45:01 +0100

    Add new function

---
+++ R/euclideanDistance3D.R
@@ -0,0 +1,3 @@
+euclideanDistance3D <-function(x1, y1, z1, x2, y2, z2){
+  return(sqrt((x1 - x2)^2 + (y1 - y2)^2 + (z1 - z2)^2))
+} # This function is used to calculate the Euclidean distance in 3D space
