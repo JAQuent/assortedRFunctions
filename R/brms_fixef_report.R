752a588c009c9b2e4f7f519d4b16dceb484d0223
JAQuent <alexander.quent@rub.de>
2019-10-11 14:49:02 +0100

    Added space and $ to string

--- R/brms_fixef_report.R
+++ R/brms_fixef_report.R
@@ -1,9 +1,9 @@
 brms_fixef_report <- function(x, decimal = 2){
-  paste('\beta = ',
+  paste('$\beta$ = ',
         round(x[1], decimal),
         ' (95 % CI [',
         round(x[3], decimal),
-        ',',
+        ', ',
         round(x[4], decimal),
         '])' ,
         sep = '')
