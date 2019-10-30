8431514c12cd66fe952fb89cd12fdfa66752da8c
JAQuent <alexander.quent@rub.de>
2019-10-30 12:24:39 +0000

    Add function that creates JSON strings with variable names.

---
+++ R/create_json_variable_str.R
@@ -0,0 +1,7 @@
+create_json_variable_str <- function(var_name, list_var){
+  require(rjson)
+  json <- toJSON(list_var)
+  Json_string <- gsub('\"', '\\\\"', json) # ading \ so that " are printed right
+  return(paste('var ', var_name,' = "', Json_string, '";', sep = ''))
+}
+
