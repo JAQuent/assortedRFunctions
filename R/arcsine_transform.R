97a187846dccbaaa168a073114aff044dabc6245
Jörn Alexander Quent <alexander.quent@rub.de>
2021-02-15 15:37:58 +0000

    Update function to reverse the transformation

--- R/arcsine_transform.R
+++ R/arcsine_transform.R
@@ -1,3 +1,11 @@
-arcsine_transform <- function(x){
-  asin(x*2 - 1)
+arcsine_transform <- function(x, reverse = FALSE){
+  if(reverse){
+    (sin(x) + 1)/2
+  } else {
+    if(x < 0 | x > 1){
+      stop('Value not between zero and one. Either forgot reverse = FALSE or wrong value. ')
+    } else {
+      asin(x*2 - 1)
+    }
+  }
 }
