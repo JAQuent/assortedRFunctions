2c3364884bf450fbf48b0365e1357c51df0c553b
Jörn Alexander Quent <alexander.quent@rub.de>
2021-03-19 16:05:57 +0000

    Making this question a dialog box

--- R/clear_environment.R
+++ R/clear_environment.R
@@ -1,20 +1,24 @@
 clear_environment <- function(){
   # This function asks whether you want the (global) environment to be cleared
 
-  # Ask
-  cat('\rDo you want the environment to be cleared? (y/n)\r')
+  # Get utils
+  require(utils)
 
-  # Get response
-  response <- readline(prompt="Response: ")
+  # Ask via winDialog
+  answer <- "NO"
+  answer <- winDialog("yesno", "Do you want your global environment to be cleared?")
 
-  if(tolower(response) == 'yes' | tolower(response) == 'y'){
+  # Use the response
+  if (answer == 'YES'){
     # Yes
-    cat('\r The environment will be cleared. \r')
+    cat('\r The environment will be cleared. \n')
 
     # Clear
     rm(list = ls(globalenv()), pos = globalenv())
   } else {
-    # No in all other cases as safety feature
-    cat('\r The environment  not will be cleared. \r')
+    cat('\r The environment  not will be cleared. \n')
   }
+
+  # Wait short time (1 second) so it is completed
+  Sys.sleep(1)
 }
