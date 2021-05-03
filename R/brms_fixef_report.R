f14d9a2a30ae9f01d756ee6c1283b36e243601ed
Jörn Alexander Quent <alexander.quent@rub.de>
2021-05-03 15:05:19 +0100

    Add signif to function

--- R/brms_fixef_report.R
+++ R/brms_fixef_report.R
@@ -1,10 +1,33 @@
-brms_fixef_report <- function(x, decimal = 2){
-  paste(' = ',
-        round(x[1], decimal),
-        ' (95 % CI [',
-        round(x[3], decimal),
-        ', ',
-        round(x[4], decimal),
-        '])' ,
-        sep = '')
+brms_fixef_report <- function(x, digits = 2, rounding_type){
+
+  # Default rounding
+  if(missing(rounding_type)){
+    rounding_type <- 'round'
+  }
+
+  # Rounding
+  if(rounding_type == 'signif'){
+    return_string <-   paste(' = ',
+                             signif(x[1], digits),
+                             ' (95 % CI [',
+                             signif(x[3], digits),
+                             ', ',
+                             signif(x[4], digits),
+                             '])' ,
+                             sep = '')
+  } else if (rounding_type == 'round'){
+    return_string <-   paste(' = ',
+                             round(x[1], digits),
+                             ' (95 % CI [',
+                             round(x[3], digits),
+                             ', ',
+                             round(x[4], digits),
+                             '])' ,
+                             sep = '')
+  } else {
+    stop('Wrong rounding type. Choose signif or round.')
+  }
+
+  # Return
+  return(return_string)
 }
