0b4867637ffbef2d8563b9b16b766f681acd703b
Jörn Alexander Quent <alexander.quent@rub.de>
2021-09-13 15:50:41 +0800

    Update arcsine_transform.R

--- R/arcsine_transform.R
+++ R/arcsine_transform.R
@@ -2,7 +2,7 @@ arcsine_transform <- function(x, reverse = FALSE){
   if(reverse){
     (sin(x) + 1)/2
   } else {
-    if(x < 0 | x > 1){
+    if(any(x < 0) | any(x > 1)){
       stop('Value not between zero and one. Either forgot reverse = FALSE or wrong value. ')
     } else {
       asin(x*2 - 1)
