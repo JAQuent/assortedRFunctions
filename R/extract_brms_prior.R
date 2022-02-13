0d5ea5fc687085b460278ce606117be24554bbd0
Jörn Alexander Quent <alexander.quent@rub.de>
2022-02-13 12:16:41 +0800

    Create extract_brms_prior.R

---
+++ R/extract_brms_prior.R
@@ -0,0 +1,10 @@
+# Prior extraction function
+extract_brms_prior <- function(brms_model_prior_string){
+  # This functions takes a string like this "student_t(7, 0, 1)"
+  # First split the string by ,
+  tempStr <- str_split(brms_model_prior_string , ",")[[1]]
+
+  # Get the numbers
+  params  <- str_extract(tempStr , "\\d+\\.*\\d*")
+  return(params)
+}
