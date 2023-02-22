e38158feab196ec2e1387b5658d559bc91a2c58b
Jörn Alexander Quent <alexander.quent@rub.de>
2023-02-22 17:09:02 +0800

    Fix cross entropy & add KL Divergence function

--- R/calc_KL_D.R
+++ R/calc_KL_D.R
@@ -1,4 +1,4 @@
-calc_cross_entropy <- function(states2analyse, predictedStates){
+calc_KL_D <- function(states2analyse, predictedStates){
   # Check if all states in states2analyse are also included in predictedStates
   if(any(!(states2analyse %in% predictedStates))){
     stop("There are states in states2analyse that are not included in predictedStates.")
@@ -23,8 +23,8 @@ calc_cross_entropy <- function(states2analyse, predictedStates){
   pj <- pj[pj > 0]
 
   # Log transform with base of 2 to make units bits
-  log_qj <- log2(qj)
+  log_qj_by_pj <- log2(qj/pj)
 
-  # Return cross-entropy value
-  return(-sum((pj*log_qj)))
+  # Return Kullback–Leibler divergence value
+  return(-sum((pj*log_qj_by_pj)))
 }
