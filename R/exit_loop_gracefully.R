245ea0386307941948ea3c4df72a15c726ab1761
Jörn Alexander Quent <alexander.quent@rub.de>
2023-03-12 15:09:27 +0800

    Update exit_loop_gracefully.R

--- R/exit_loop_gracefully.R
+++ R/exit_loop_gracefully.R
@@ -1,4 +1,4 @@
-exit_loop_gracefully <- function(fileName) {
+exit_loop_gracefully <- function(fileName, startIndex, i) {
   cat("\nSaving results so far...\n")
   cat("Start   index =", startIndex, "\n")
   cat("Current index =", i, "\n")
