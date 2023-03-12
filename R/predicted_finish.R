d0f4b25927774d34ac3ca53f3236cd9f65bf2c71
Jörn Alexander Quent <alexander.quent@rub.de>
2023-03-12 13:10:25 +0800

    Create predicted_finish.R

---
+++ R/predicted_finish.R
@@ -0,0 +1,17 @@
+predicted_finish <- function(startIndex, i, n, startTime){
+  # Get current time
+  now <- Sys.time()
+
+  # Get the time that elapsed since the start
+  elapsedTime       <- difftime(now, startTime, units = 'hours')
+  # Estimate how long one iteration takes
+  timePerIter       <- elapsedTime/(i - startIndex + 1)
+  # Calculate how many iterations are left before completion
+  iterLeft          <- n - i
+  # Estimate how many hours are left and add this to current time
+  predictedTimeLeft   <- timePerIter * iterLeft
+  predictedFinishTime <- now + predictedTimeLeft
+
+  # Print results to the console
+  cat(paste0("\n Iteration: ", i,"/", n, " | The process has ", signif(predictedTimeLeft, 4), " hours left | Predicted finished: ", predictedFinishTime))
+}
