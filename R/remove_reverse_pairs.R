082a568945206686df396707fb54214d4bb8cc68
Jörn Alexander Quent <alexander.quent@rub.de>
2023-04-19 13:12:02 +0800

    Fix typo

--- R/remove_reverse_pairs.R
+++ R/remove_reverse_pairs.R
@@ -2,12 +2,12 @@ remove_reverse_pairs <- function(df){
   # Df must be created like df <- expand.grid(var, var)
   # Keep A B but remove B A pairs
   df$id <- 1:nrow(df)
-  df <- ddply(df, c("id"), mutate, dub_finder  = paste(sort(c(Var1, Var2)), collapse = "_"))
-  df <- df[!duplicated(df$dub_finder), ]
+  df <- ddply(df, c("id"), mutate, dup_finder  = paste(sort(c(Var1, Var2)), collapse = "_"))
+  df <- df[!duplicated(df$dup_finder), ]
 
-  # Remove ID and dub_finder
+  # Remove ID and dup_finder
   df$id <- NULL
-  df$dub_finder <- NULL
+  df$dup_finder <- NULL
 
   # Return
   return(df)
