532f2e0b8b1c52bcd9e09792fb2976297fb85c83
Jörn Alexander Quent <alexander.quent@rub.de>
2023-05-12 11:12:13 +0800

    Create delete_everything_in_folder.R

---
+++ R/delete_everything_in_folder.R
@@ -0,0 +1,25 @@
+delete_everything_in_folder <- function(path2folder, verbose = TRUE){
+  # Check if the path ends on /
+  lastChar <- substr(path2folder, nchar(path2folder), nchar(path2folder))
+  if(lastChar == "/"){
+    # Remove
+    path2folder <- substr(path2folder, 1, nchar(path2folder)-1)
+  }
+
+
+  # List all files
+  allFiles   <- list.files(path2folder, recursive = TRUE)
+  allFolders <- list.dirs(path2folder, recursive = TRUE)
+
+  # Exclude folder itself
+  allFolders <- allFolders[allFolders != path2folder]
+
+  # Remove all
+  unlink(paste0(path2folder, "/", allFiles), recursive = TRUE)
+  unlink(allFolders, recursive = TRUE)
+
+  # Report
+  if(verbose){
+    cat("\n ", length(allFiles), " filea(s) and ", length(allFolders), " folder(s) were deleted. \n")
+  }
+}
