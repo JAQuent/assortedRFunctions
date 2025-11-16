c30f743896cdd60b5ef1f6c6cb17eb9bf0842257
JAQuent <alexander.quent@rub.de>
2025-11-17 07:15:44 +0800

    Version 1.3.0: add option to convert outlier to boolean to mad_outlier

--- R/mad_outlier.R
+++ R/mad_outlier.R
@@ -1,14 +1,19 @@
-mad_outlier <- function(x, times = 2){
+mad_outlier <- function(x, times = 2, bool = FALSE){
   # This function calculates the Median Absolute Deviation to detect outlier
-  
+
   # Calculate lower and up limit with median and mad
   ll <- median(x) - times*mad(x)
   ul <- median(x) + times*mad(x)
-  
+
   # Detect outlier
   outlier <- rep(0, length(x))
   outlier[x < ll] <- 1
   outlier[x > ul] <- 1
-  
+
+  # Convert to bool
+  if(bool){
+    outlier <- ifelse(outlier == 1, TRUE, FALSE)
+  }
+
   return(outlier)
 }
