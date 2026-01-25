556d6f963f2972261d51f4f309d0df9f8bfc16c7
JAQuent <alexander.quent@rub.de>
2026-01-25 17:45:10 +0800

    Version 1.6.0: Add create_HCP_91k_xii_template()

---
+++ R/create_HCP_91k_xii_template.R
@@ -0,0 +1,15 @@
+# Function to create a HCP-style 32k
+create_HCP_91k_xii_template <- function(){
+  # Create load ones_1k.dscalar.nii to get necessary stuff for subcortical data
+  sub_data <- read_cifti(ciftiTools.files()$cifti["dscalar_ones"])
+
+  # Create a new xifti with 1, 2 and 3
+  template_xii <- as.xifti(cortexL = rep(0, 29696),
+                           cortexR = rep(1, 29716),
+                           subcortVol = as.matrix(rep(2, 31870)),
+                           subcortMask = sub_data$meta$subcort$mask,
+                           subcortLabs = sub_data$meta$subcort$labels)
+
+  # Return
+  return(template_xii)
+}
