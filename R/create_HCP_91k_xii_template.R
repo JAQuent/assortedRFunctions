417a6e30441c562e835d298c3f0c8bf39f979103
JAQuent <alexander.quent@rub.de>
2026-01-25 18:01:15 +0800

    Further improve/fix comment

--- R/create_HCP_91k_xii_template.R
+++ R/create_HCP_91k_xii_template.R
@@ -1,4 +1,4 @@
-# Function to create a HCP-style 32k
+# Function to create a HCP-style 91k xifti object
 create_HCP_91k_xii_template <- function(){
   # Create load ones_1k.dscalar.nii to get necessary stuff for subcortical data
   sub_data <- read_cifti(ciftiTools.files()$cifti["dscalar_ones"])
