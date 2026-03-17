2ddb0918d94f90ff29aa4f52a321c1413a6403ce
JAQuent <alexander.quent@rub.de>
2026-03-17 12:50:48 +0800

    Add a pre string (e.g β) to brms_fixef_report

--- R/brms_fixef_report.R
+++ R/brms_fixef_report.R
@@ -1,29 +1,32 @@
-brms_fixef_report <- function(x, digits = 2, rounding_type){
+brms_fixef_report <- function(x, digits = 2, rounding_type, pre_string){
 
   # Default rounding
   if(missing(rounding_type)){
     rounding_type <- 'round'
   }
 
+  # Default pre string
+  if(missing(pre_string)){
+    pre_string <- ''
+  }
+
   # Rounding
   if(rounding_type == 'signif'){
-    return_string <-   paste(' = ',
+    return_string <-   paste0(pre_string, ' = ',
                              signif(x[1], digits),
                              ' (95 % CI [',
                              signif(x[3], digits),
                              ', ',
                              signif(x[4], digits),
-                             '])' ,
-                             sep = '')
+                             '])' )
   } else if (rounding_type == 'round'){
-    return_string <-   paste(' = ',
+    return_string <-   paste0(pre_string, ' = ',
                              round(x[1], digits),
                              ' (95 % CI [',
                              round(x[3], digits),
                              ', ',
                              round(x[4], digits),
-                             '])' ,
-                             sep = '')
+                             '])')
   } else {
     stop('Wrong rounding type. Choose signif or round.')
   }
